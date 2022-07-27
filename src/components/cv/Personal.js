6591565db7f3c73140d3fae94b260fdbcd85d54b
CalAnizoba <obinnaanizoba5@gmail.com>
2022-07-27 21:15:01 +0800

    Refractor the component

--- src/components/cv/Personal.js
+++ src/components/cv/Personal.js
@@ -1,130 +1,61 @@
-import React, { Component } from "react";
-import uniqid from "uniqid";
+import React, { useState } from "react";
 import Preview from "../cv-preview/PreviewPersonal-info";
-class Personal extends Component {
-    constructor() {
-        super()
-        this.state = {
-            personalName: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            personalEmail: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            personalPhone: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            PersonalInfo: [],
-            mode: true
-        };
-    }
+import uniqid from "uniqid";
 
+const Personal = () => {
+    const initialPersonalInfo = {
+        name: "",
+        email: "",
+        phone: "",
+        id: uniqid()
+    };
 
-    handleNameChange = (e) => {
-        this.setState({
-            personalName: {
-                name: e.target.value,
-                id: this.state.personalName.id
-            }
-        });
-    }
+    const [personal, setPersonal] = useState(initialPersonalInfo);
+    const [personalInfo, setPersonalInfo] = useState([]);
+    const [mode, setMode] = useState(true);
+    const [reset, setReset] = useState("");
 
-    handleEmailChange = (e) => {
-        this.setState({
-            personalEmail: {
-                name: e.target.value,
-                id: this.state.personalEmail.id
-            }
-        });
-    }
-    handlePhoneChange = (e) => {
-        this.setState({
-            personalPhone: {
-                name: e.target.value,
-                id: this.state.personalPhone.id
-            }
+    const handleChange = (e) => {
+        setPersonal({
+            ...personal,
+            [e.target.name]: e.target.value,
         });
     };
 
-    onSubmit = (e) => {
+    const addPersonalInfo = ()=>{
+        setPersonalInfo([...personalInfo, personal]);
+        setMode(false);
+    }
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const { personalName, personalEmail, personalPhone, PersonalInfo } = this.state;
-        this.setState({
-            PersonalInfo: PersonalInfo.concat(personalName, personalEmail, personalPhone),
-            personalName: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            personalEmail: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            personalPhone: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            mode: false
-        });
+        setPersonal(addPersonalInfo);
+        setReset(reset);
+        console.log(personal);
     };
 
-    handleEditing = () => {
-        this.setState({
-            editing: true,
-        });
-    }
 
-    setUpdate = (updatedText, id) => {
-        const { PersonalInfo } = this.state
-        this.setState({
-            personalInfo: PersonalInfo.map((person) => {
-                if (person.id === id) {
-                    person.name = updatedText;
-                }
-                return person;
-            })
-        });
-    }
-
-    handleUpdate = (e) => {
-        if (e.key === "Enter") {
-            this.setState({
-                editing: false
-            })
-        }
-    }
-
-
-    render() {
-        const { personalName, personalEmail, personalPhone, PersonalInfo, mode, editing } = this.state;
-        return mode ? (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <div className="button-container">
-                        <button className="button-submit">Preview Personal Info</button>
-                    </div>
-                    <div className="personal-info">
-                        <label>Name: </label>
-                        <input type="text" value={personalName.name} onChange={this.handleNameChange}></input>
-                    </div>
-                    <div className="personal-info">
-                        <label>Email: </label>
-                        <input type="email" value={personalEmail.name} onChange={this.handleEmailChange}></input>
-                    </div>
-                    <div className="personal-info">
-                        <label>Phone:</label>
-                        <input type="tel" value={personalPhone.name} onChange={this.handlePhoneChange}></input>
-                    </div>
-                </form>
-            </div>) : (<Preview PersonalInfo={PersonalInfo} handleEditing={this.handleEditing} editing={editing} setUpdate={this.setUpdate} handleUpdate={this.handleUpdate} />);
-    };
+    return mode ? (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="button-container">
+                    <button className="button-submit">Preview Personal Info</button>
+                </div>
+                <div className="personal-info">
+                    <label>Name: </label>
+                    <input type="text" name="name" value={personal.name} onChange={handleChange}></input>
+                </div>
+                <div className="personal-info">
+                    <label>Email: </label>
+                    <input type="email" name="email" value={personal.email} onChange={handleChange}></input>
+                </div>
+                <div className="personal-info">
+                    <label>Phone:</label>
+                    <input type="tel" name="phone" value={personal.phone} onChange={handleChange}></input>
+                </div>
+            </form>
+        </div>) : (
+        <Preview personal={personalInfo} mode={mode}/>
+    )
 }
 export default Personal;
