e5afc9fc1365904a47daa9eefae23a68f0ed1952
CalAnizoba <obinnaanizoba5@gmail.com>
2022-07-28 17:19:20 +0800

    finished up the edit button functionality

--- src/components/cv/Education.js
+++ src/components/cv/Education.js
@@ -1,149 +1,70 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 import PreviewEducation from "../cv-preview/PreviewEducation-info";
-class Education extends Component {
-    constructor() {
-        super()
-        this.state = {
-            school: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            study: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            studyDate: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            studyEnd: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            EducationInfo: [],
-            mode: true
-        };
-    }
 
-    handleSchoolChange = (e) => {
-        this.setState({
-            school: {
-                name: e.target.value,
-                id: this.state.school.id
-            },
-        });
-    }
-    handleStudyTitleChange = (e) => {
-        this.setState({
-            study: {
-                name: e.target.value,
-                id: this.state.study.id
-            },
-        });
-    }
-    handleStudyDateChange = (e) => {
-        this.setState({
-            studyDate: {
-                name: e.target.value,
-                id: this.state.studyDate.id
-            },
-        });
-    }
-    handleStudyDateEndChange = (e) => {
-        this.setState({
-            studyEnd: {
-                name: e.target.value,
-                id: this.state.studyEnd.id
-            },
-        });
-    }
+const Education = (props) => {
+    const {schoolName, studyTitle, studyDate, studyEndDate} = props;
 
-    onSubmit = (e) => {
+
+    const initialEducationInfo = {
+        schoolName: schoolName || "",
+        studyTitle: studyTitle || "",
+        studyDate: studyDate || "",
+        studyEndDate: studyEndDate || "",
+        id: uniqid()
+    };
+
+    const [education, setEducation] = useState(initialEducationInfo);
+    const [Education, setEducationInfo] = useState([]);
+    const [modeEducation, setMode] = useState(true)
+
+    const handleChange = (e) => {
+        setEducation({
+            ...education,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const addEducationInfo = () => {
+        setEducationInfo([...Education, education]);
+    };
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const { school, study, studyDate, studyEnd, EducationInfo } = this.state
-        this.setState({
-            EducationInfo: EducationInfo.concat(school, study, studyDate, studyEnd),
-            school: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            study: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            studyDate: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            studyEnd: {
-                name: "",
-                id: uniqid(),
-                editing: false
-            },
-            mode: false
-        })
-    }
-    handleEditing = () => {
-        this.setState({
-            editing: true,
-        });
-    }
+        setEducation(addEducationInfo);
+        setMode(false);
+        setEducation(initialEducationInfo);
+        console.log(education);
+    };
 
-    setUpdate = (updatedText, id) => {
-        const { EducationInfo } = this.state
-        this.setState({
-            EducationInfo: EducationInfo.map((education) => {
-                if (education.id === id) {
-                    education.name = updatedText;
-                }
-                return education;
-            })
-        });
-    }
+    return modeEducation ? (
 
-    handleUpdate = (e) => {
-        if (e.key === "Enter") {
-            this.setState({
-                editing: false
-            })
-        }
-    }
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="button-container">
+                    <button className="button-submit">Preview Education Info</button>
+                </div>
+                <div className="educational-info">
+                    <label>School Name: </label>
+                    <input type="text" name="schoolName" value={education.schoolName} onChange={handleChange}></input>
+                </div>
+                <div className="educational-info">
+                    <label>Title of Study: </label>
+                    <input type="text" name="studyTitle" value={education.studyTitle} onChange={handleChange}></input>
+                </div>
+                <div className="educational-info">
+                    <label>Date of Study from: </label>
+                    <input type="date" name="studyDate" value={education.studyDate} onChange={handleChange}></input>
+                </div>
+                <div className="educational-info">
+                    <label>Date of Study to: </label>
+                    <input type="date" name="studyEndDate" value={education.studyEndDate} onChange={handleChange}></input>
+                </div>
+            </form>
+        </div>) : (
+        <PreviewEducation education={Education} />
+    )
 
-    render() {
-        const { school, study, studyDate, studyEnd, EducationInfo, mode, editing } = this.state;
-        return mode ? (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <div className="button-container">
-                        <button className="button-submit">Preview Education Info</button>
-                    </div>
-                    <div className="educational-info">
-                        <label>School Name: </label>
-                        <input type="text" value={school.name} onChange={this.handleSchoolChange}></input>
-                    </div>
-                    <div className="educational-info">
-                        <label>Title of Study: </label>
-                        <input type="text" value={study.title} onChange={this.handleStudyTitleChange}></input>
-                    </div>
-                    <div className="educational-info">
-                        <label>Date of Study from: </label>
-                        <input type="date" value={studyDate.date} onChange={this.handleStudyDateChange}></input>
-                    </div>
-                    <div className="educational-info">
-                        <label>Date of Study to: </label>
-                        <input type="date" value={studyEnd.dateEnd} onChange={this.handleStudyDateEndChange}></input>
-                    </div>
-                </form>
-            </div>) : (<PreviewEducation EducationInfo={EducationInfo} handleEditing={this.handleEditing} editing={editing} setUpdate={this.setUpdate} handleUpdate={this.handleUpdate} />)
-    }
 }
 
 export default Education;
